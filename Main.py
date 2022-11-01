3ed5113fc0f9383b9bb3ac14368ec50ef865b574
URK21CS2048 <109647908+URK21CS2048@users.noreply.github.com>
2022-11-01 20:02:43 +0530

    ex 4b

--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
 length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
-# Write your code here
+# Write your code here 
 actual_list = []
- 
-    
+
 value = 0
 while len(actual_list) < length_of_circular_linked_list and value < len(circular_linked_list):
     element = circular_linked_list[value]
